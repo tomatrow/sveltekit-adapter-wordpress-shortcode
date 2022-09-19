88c097402ef897c5a3e67e83f10d431e3a189298
AJ Caldwell <tomatrow@gmail.com>
2022-09-18 19:27:03 -0700

    Upgrade adapter

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 import { Adapter } from "@sveltejs/kit"
 
 interface AdapterOptions {
-    pages?: string
-    assets?: string
-    fallback?: string
-    indexPath?: string
-    shortcode?: string
-    shadow?: boolean
-	renderHead?: (head: HTMLHeadElement) => string,
+	pages?: string
+	assets?: string
+	fallback?: string
+	indexPath?: string
+	shortcode?: string
+	shadow?: boolean
+	renderHead?: (head: HTMLHeadElement) => string
 	renderBody?: (head: HTMLBodyElement) => string
 }
 
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { readFileSync, writeFileSync } from "fs"
 import { resolve } from "path"
 import { JSDOM } from "jsdom"
 
-const files = fileURLToPath(new URL('./files', import.meta.url).href);
+const files = fileURLToPath(new URL("./files", import.meta.url).href)
 
 /** @type {import('.')} */
 export default function ({
@@ -13,27 +13,21 @@ export default function ({
 	indexPath = "index.php",
 	shadow = false,
 	shortcode = "svelte-kit-shortcode",
-	renderHead = head => 
+	renderHead = head =>
 		[...head.querySelectorAll(`link[rel="modulepreload"]`)]
 			.map(element => element.outerHTML)
-			.join("")
-	,
+			.join(""),
 	renderBody = body => body.innerHTML
 } = {}) {
 	return {
-		name: "@sveltejs/adapter-wordpress-shortcode",
+		name: "sveltekit-adapter-wordpress-shortcode",
 
 		async adapt(builder) {
-			if (!fallback && !builder.config.kit.prerender.default) 
-				builder.log.warn(
-					'You should set `config.kit.prerender.default` to `true` if no fallback is specified'
-				)
-
 			if (!builder.config.kit.paths.base)
 				builder.log.warn(
 					"You should set config.kit.paths.base to something like `/wp-content/plugins/my-shortcode-plugin`"
 				)
-			
+
 			if (!builder.config.kit.paths.assets)
 				builder.log.warn(
 					"You should set config.kit.paths.assets to something like `https://example.com/wp-content/plugins/my-shortcode-plugin`"
@@ -44,6 +38,16 @@ export default function ({
 
 			builder.writeClient(assets)
 			builder.writePrerendered(pages, { fallback })
+			for (const { file } of [...builder.prerendered.pages.values()])
+				if (file !== "index.html") builder.rimraf(resolve(pages, file))
+
+			if (builder.prerendered.pages.get("/")?.file !== "index.html") {
+				builder.log.error(
+					`sveltekit-adapter-wordpress-shortcode: root route must be prerendered (unless using the 'fallback' option — see https://github.com/sveltejs/kit/tree/master/packages/adapter-static#spa-mode). Try adding \`export const prerender = true\` to your root layout.js — see https://kit.svelte.dev/docs/page-options#prerender for more details`
+				)
+
+				throw new Error("Encountered non rendered root route")
+			}
 
 			// copy php files
 			builder.copy(indexPath, resolve(pages, "index.php"))
@@ -58,13 +62,17 @@ export default function ({
 			const indexHtmlPath = resolve(pages, "index.html")
 			const dom = new JSDOM(readFileSync(indexHtmlPath, "utf8"))
 			builder.rimraf(indexHtmlPath)
-			writeFileSync(resolve(pages, "svelte_kit_shortcode_head.html"), renderHead(dom.window.document.head))
-			writeFileSync(resolve(pages, "svelte_kit_shortcode_body.html"), renderBody(dom.window.document.body))
+			writeFileSync(
+				resolve(pages, "svelte_kit_shortcode_head.html"),
+				renderHead(dom.window.document.head)
+			)
+			writeFileSync(
+				resolve(pages, "svelte_kit_shortcode_body.html"),
+				renderBody(dom.window.document.body)
+			)
 
-			if (pages === assets) 
-				builder.log(`Wrote site to "${pages}"`)
-			 else 
-				builder.log(`Wrote pages to "${pages}" and assets to "${assets}"`)
+			if (pages === assets) builder.log(`Wrote site to "${pages}"`)
+			else builder.log(`Wrote pages to "${pages}" and assets to "${assets}"`)
 		}
 	}
 }
